f4cd2b4127279b0a407afa213e2ab6d67c9830cf
= <cooneypaul1997@gmail.com>
2025-12-01 23:45:58 +0000

    Day 1 Part 2 Working!

--- 2025/day01/2025_day1.py
+++ 2025/day01/2025_day1.py
@@ -5,18 +5,18 @@ with open("real-input.txt", mode="r") as file:
     read_data = file.read().splitlines()
 
     for line in read_data:
-        if line[0:1] == "L":
-            move_amount = int(line[1:])
-            starting_point = (starting_point - move_amount) % 100
-
-            if starting_point == 0:
-                zero_counter += 1
+        amount = int(line[1:])
 
         if line[0:1] == "R":
-            move_amount = int(line[1:])
-            starting_point = (starting_point + move_amount) % 100
+            for _ in range(amount):
+                starting_point = (starting_point + 1) % 100
+                if starting_point == 0:
+                    zero_counter += 1
 
-            if starting_point == 0:
-                zero_counter += 1
+        if line[0:1] == "L":
+            for _ in range(amount):
+                starting_point = (starting_point - 1) % 100
+                if starting_point == 0:
+                    zero_counter += 1
 
-print(f"{zero_counter}")
+print(zero_counter)
