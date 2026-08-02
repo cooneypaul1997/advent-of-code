c72708659414b35df5290e2401ce49b2ab82e424
Paul Cooney <paul-cooney97@outlook.com>
2026-08-02 22:51:06 +0100

    2015 Day 1 Parts 1 & 2

---
+++ 2015/day01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+
+string path = Path.Combine(Directory.GetCurrentDirectory(), "input.txt");
+
+string contents = File.ReadAllText(path);
+
+int counter = 0;
+
+// Part 1
+foreach (char c in contents)
+{
+    if (c == '(')
+    {
+        counter++;
+    }
+    else if (c == ')')
+    {
+        counter--;
+    }
+}
+
+Console.WriteLine(counter);
+
+// Part 2
+int position = 0;
+int counter2 = 0;
+int index = 0;
+
+foreach (char c in contents)
+{
+    index++;
+
+    if (c == '(')
+    {
+        counter2++;
+    }
+    else if (c == ')')
+    {
+        counter2--;
+
+        if (counter2 < 0)
+        {
+            position = index;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(position);
